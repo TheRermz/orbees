8e434d67ee3ba0e979b33067294428aac721659f
TheRermz <mufischer@hotmail.com>
2026-03-25 14:26:15 -0300

    feat(api): Cria o service de autenticação

---
+++ orbees-api/Services/Auth/AuthService.cs
@@ -0,0 +1,32 @@
+using Api.Dtos;
+using Api.Services.Interfaces.Auth;
+
+namespace Api.Services.Auth
+{
+    public class AuthService(
+        IAuthRegisterService registerService,
+        IAuthLoginService loginService,
+        IAuthEmailConfirmService emailConfirmService,
+        IAuthPasswordService passwordService,
+        IAuthOAuthService oAuthService
+        ) : IAuthService
+    {
+        public Task<UserReadDto> RegisterAsync(UserCreateDto dto) =>
+          registerService.RegisterAsync(dto);
+
+        public Task<string> LoginAsync(string email, string password) =>
+          loginService.LoginAsync(email, password);
+
+        public Task ConfirmEmailAsync(string token) =>
+          emailConfirmService.ConfirmEmailAsync(token);
+
+        public Task ForgotPasswordAsync(string email) =>
+          passwordService.ForgotPasswordAsync(email);
+
+        public Task ResetPasswordAsync(string token, string newPassword) =>
+          passwordService.ResetPasswordAsync(token, newPassword);
+
+        public Task<string> HandleGoogleCallbackAsync(string email, string name, string providerId) =>
+          oAuthService.HandleGoogleCallbackAsync(email, name, providerId);
+    }
+}
