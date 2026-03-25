bc5ec3fc319e6d66b7ce3967408e62e4ac7e9373
TheRermz <mufischer@hotmail.com>
2026-03-25 10:14:13 -0300

    feat(api): Cria Interface e Service de confirmação de email

---
+++ orbees-api/Services/Auth/AuthEmailConfirmService.cs
@@ -0,0 +1,27 @@
+using Api.Repositories.Interfaces;
+using Api.Services.Interfaces;
+
+namespace Api.Services.Auth
+{
+    public class AuthEmailConfirmService(
+        IUserRepository userRepository
+        ) : IAuthEmailConfirmService
+    {
+        public async Task ConfirmEmailAsync(string token)
+        {
+            var user = await userRepository.GetByEmailConfirmationTokenAsync(token)
+              ?? throw new InvalidOperationException("Token inválido ou expirado.");
+
+            if (user.EmailConfirmationExpiresAt < DateTime.UtcNow)
+                throw new InvalidOperationException("Token Inválido. Solicite um novo.");
+
+            user.EmailConfirmed = true;
+            user.EmailConfirmationToken = null;
+            user.EmailConfirmationExpiresAt = null;
+            user.UpdatedAt = DateTime.UtcNow;
+
+            await userRepository.UpdateAsync(user);
+            await userRepository.SaveChangesAsync();
+        }
+    }
+}
