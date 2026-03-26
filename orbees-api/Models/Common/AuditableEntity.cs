72ec4503f7416526449bd013010d2f9f55d69dcd
Murilo <mufischer@hotmail.com>
2026-03-25 22:12:41 -0300

    refactor(api): Deixa o UpdatedAt como opcional

--- orbees-api/Models/Common/AuditableEntity.cs
+++ orbees-api/Models/Common/AuditableEntity.cs
@@ -4,6 +4,6 @@ namespace Api.Models.Common
     public abstract class AuditableEntity
     {
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
