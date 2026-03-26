df48be08f80ba6bd831f0488e9efd7ab38f2b8df
Murilo <mufischer@hotmail.com>
2026-03-25 22:13:46 -0300

    chore(api): Modifica a criação e atualização do created_at e updated_at

--- orbees-api/Data/ApiDbContext.cs
+++ orbees-api/Data/ApiDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Api.Models;
 using System.Reflection;
+using Api.Models.Common;
 
 
 namespace Api.Data
@@ -10,6 +11,22 @@ namespace Api.Data
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; } = null!;
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<AuditableEntity>();
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Entity.CreatedAt = DateTime.UtcNow;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
