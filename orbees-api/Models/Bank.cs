3fe40c18861258de2e7350e74eb344a18f7a3a5c
TheRermz <mufischer@hotmail.com>
2026-03-27 12:00:52 -0300

    feat(bank): Cria models de conta bancária e un. do banco

---
+++ orbees-api/Models/Bank.cs
@@ -0,0 +1,14 @@
+using Api.Models.Common;
+
+namespace Api.Models
+{
+    public class Bank : AuditableEntity
+    {
+        public int Id { get; set; }
+        public required string BankName { get; set; }
+        public required string BankCode { get; set; } // Código COMPE
+        public string? Ispb { get; set; }             // ex: 18236120
+        public string? CsvHeaderSignature { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
