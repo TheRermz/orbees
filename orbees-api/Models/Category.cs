9a19efc2fdbbb278b71b67355e3791f75c21bc4a
TheRermz <mufischer@hotmail.com>
2026-03-27 12:04:45 -0300

    feat(category): Adiciona model de categorias

---
+++ orbees-api/Models/Category.cs
@@ -0,0 +1,19 @@
+using Api.Models.Common;
+
+namespace Api.Models
+{
+    public class Category : AuditableEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Name { get; set; }
+        public string? Icon { get; set; }
+        public string? Color { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        // FK USER -- null se for categoria padrão
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+
+        // FK Grupo
+    }
+}
