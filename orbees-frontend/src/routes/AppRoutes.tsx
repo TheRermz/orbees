abf741af42efa1104594753893383d1bf30fa0c4
Murilo <mufischer@hotmail.com>
2026-05-22 17:30:48 -0300

    feat(routes): adiciona mapeamento das rotas (sujeito a alterações)

---
+++ orbees-frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,101 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
+
+// rotas das páginas
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* públicas */}
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <h1>Login</h1>
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute>
+              <h1>Register</h1>
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <PublicRoute>
+              <h1>Forgot Password</h1>
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/reset-password"
+          element={
+            <PublicRoute>
+              <h1>Reset Password</h1>
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/not-found"
+          element={
+            <PublicRoute>
+              <h1>Not Found</h1>
+            </PublicRoute>
+          }
+        />
+
+        {/* privadas */}
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <h1>Dashboard</h1>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/transactions"
+          element={
+            <PrivateRoute>
+              <h1>Transactions</h1>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/categories"
+          element={
+            <PrivateRoute>
+              <h1>Categories</h1>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/groups"
+          element={
+            <PrivateRoute>
+              <h1>Groups</h1>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <h1>Profile</h1>
+            </PrivateRoute>
+          }
+        />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
