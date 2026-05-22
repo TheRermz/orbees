a7f75c6eda8c13f300e74cb058bbd68d2792fd7e
Murilo <mufischer@hotmail.com>
2026-05-22 17:32:07 -0300

    refactor(main): Ajusta a main para context api

--- orbees-frontend/src/main.tsx
+++ orbees-frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { AuthProvider } from "./contexts/AuthProvider.tsx";
+import { AppRoutes } from "./routes/AppRoutes.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  </StrictMode>
+);
