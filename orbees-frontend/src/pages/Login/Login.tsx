dc04a056750e622d8ea0b78495032815f1f9fd23
TheRermz <mufischer@hotmail.com>
2026-05-25 09:12:14 -0300

    fix(services): Ajusta o nome da ENV

--- orbees-frontend/src/pages/Login/Login.tsx
+++ orbees-frontend/src/pages/Login/Login.tsx
@@ -37,7 +37,7 @@ export const LoginPage = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = `${import.meta.env.VITE_API_URL}/auth/google`;
+    window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/google`;
   };
 
   return (
