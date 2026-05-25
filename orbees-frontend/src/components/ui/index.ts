69657fcd5845b3a9d52008d31821344e22bedc58
TheRermz <mufischer@hotmail.com>
2026-05-25 09:44:40 -0300

    feat(components/ui): Adiciona o Modal no export do index

--- orbees-frontend/src/components/ui/index.ts
+++ orbees-frontend/src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { Input } from "./Input/Input";
 export { Divider } from "./Divider/Divider";
 export { ErrorMessage } from "./ErrorMessage/ErrorMessage";
 export { SuccessMessage } from "./SuccessMessage/SuccessMessage";
+export { Modal } from "./../Modal/Modal";
