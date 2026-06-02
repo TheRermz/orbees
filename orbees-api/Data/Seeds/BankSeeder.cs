5f9b87cb483e20d211fb99758633c69919dde8e5
TheRermz <mufischer@hotmail.com>
2026-06-02 13:13:40 -0300

    refactor(banks): Bancos não implementados foram estão como IsActive = false

--- orbees-api/Data/Seeds/BankSeeder.cs
+++ orbees-api/Data/Seeds/BankSeeder.cs
@@ -11,13 +11,13 @@ namespace Api.Data.Seeds
 
             var banks = new List<Bank>
             {
-              new() { BankName = "Nubank",                  BankCode = "260", Ispb = "18236120" },
-              new() { BankName = "Itaú Unibanco",           BankCode = "341", Ispb = "60701190" },
-              new() { BankName = "Bradesco",                BankCode = "237", Ispb = "60746948" },
-              new() { BankName = "Banco do Brasil",         BankCode = "001", Ispb = "00000000" },
-              new() { BankName = "Caixa Econômica Federal", BankCode = "104", Ispb = "36074118" },
-              new() { BankName = "Santander",               BankCode = "033", Ispb = "90400888" },
-              new() { BankName = "Banco Inter",             BankCode = "077", Ispb = "00416968" },
+              new() { BankName = "Nubank",                  BankCode = "260", Ispb = "18236120", IsActive = true },
+              new() { BankName = "Itaú Unibanco",           BankCode = "341", Ispb = "60701190", IsActive = false }, // Não implementado
+              new() { BankName = "Bradesco",                BankCode = "237", Ispb = "60746948", IsActive = false }, // Não implementado
+              new() { BankName = "Banco do Brasil",         BankCode = "001", Ispb = "00000000", IsActive = false }, // Não implementado
+              new() { BankName = "Caixa Econômica Federal", BankCode = "104", Ispb = "36074118", IsActive = false }, // Não implementado
+              new() { BankName = "Santander",               BankCode = "033", Ispb = "90400888", IsActive = true },
+              new() { BankName = "Banco Inter",             BankCode = "077", Ispb = "00416968", IsActive = false }, // Não implementado
             };
 
             await context.Banks.AddRangeAsync(banks);
